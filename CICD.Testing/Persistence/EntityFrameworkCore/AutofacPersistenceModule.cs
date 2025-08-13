b560b6fc30325e1961b17df48e48a010264d2665
Emirhan Doğan <emirhan.dogan@vendorside.com>
2025-08-13 22:07:17 +0300

    create context and using autofac.

---
+++ CICD.Testing/Persistence/EntityFrameworkCore/AutofacPersistenceModule.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Persistence.EntityFrameworkCore.DbContexts;
+
+namespace Persistence.EntityFrameworkCore
+{
+    public class AutofacPersistenceModule : Autofac.Module
+    {
+        private readonly IConfiguration _configuration;
+
+        public AutofacPersistenceModule(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.Register(c =>
+            {
+                var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+                optionsBuilder.UseNpgsql(_configuration.GetConnectionString("DefaultConnection"));
+
+                var context = new AppDbContext(optionsBuilder.Options);
+                context.Database.Migrate();
+                return context;
+            }).AsSelf().InstancePerLifetimeScope();
+
+            
+        }
+
+    }
+}
--- CICD.Testing/WebAPI/Program.cs
+++ CICD.Testing/WebAPI/Program.cs
@@ -1,15 +1,21 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Persistence.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder =>
+{
+    containerBuilder.RegisterModule(new AutofacPersistenceModule(builder.Configuration));
+});
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
