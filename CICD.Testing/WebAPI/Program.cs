0667750096aa60c87835578ade90fa666a6f721b
Emirhan Doğan <emirhan.dogan@vendorside.com>
2025-08-13 22:13:19 +0300

    register DI container.

--- CICD.Testing/WebAPI/Program.cs
+++ CICD.Testing/WebAPI/Program.cs
@@ -1,6 +1,10 @@
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
+using Core.Persistence.EntityFrameworkCore.Interfaces;
+using Core.Persistence.EntityFrameworkCore;
 using Persistence.EntityFrameworkCore;
+using Core.CQRS;
+using Core.CQRS.RequestHandler;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +12,18 @@ builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
 builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder =>
 {
     containerBuilder.RegisterModule(new AutofacPersistenceModule(builder.Configuration));
+    containerBuilder.RegisterType<UnitOfWork>()
+                    .As<IUnitOfWork>()
+                    .InstancePerLifetimeScope();
+    containerBuilder.RegisterType<Mediator>()
+                    .As<IMediator>()
+                    .InstancePerLifetimeScope();
+    containerBuilder.RegisterAssemblyTypes(AppDomain.CurrentDomain.GetAssemblies())
+        .AsClosedTypesOf(typeof(IRequestHandler<,>))
+        .InstancePerLifetimeScope();
+    containerBuilder.RegisterAssemblyTypes(AppDomain.CurrentDomain.GetAssemblies())
+        .AsClosedTypesOf(typeof(INotificationHandler<>))
+        .InstancePerLifetimeScope();
 });
 
 builder.Services.AddControllers();
